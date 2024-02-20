4b646e1ae549512cbf98c8d920e60858a98b27e2
Anthony Washington <awashington251@gmail.com>
2024-02-20 00:10:00 -0500

    create docker image, ecr and security groups for cluster and alb

--- graphql-infra/pulumi.ts
+++ graphql-infra/pulumi.ts
@@ -1,7 +1,89 @@
 import * as aws from '@pulumi/aws';
+import * as docker from '@pulumi/docker';
+import * as pulumi from '@pulumi/pulumi';
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket('my-bucket');
+const albAllowInboundFromAnywhere = new aws.ec2.SecurityGroup(
+  'alb-allow-inbound-from-anywhere',
+  {
+    name: 'alb-allow-inbound-from-anywhere',
+    description: 'Application load balancer allow traffic from anywhere',
+    ingress: [
+      {
+        // Allow inbound HTTP traffic from any IP address
+        protocol: 'TCP',
+        cidrBlocks: ['0.0.0.0/0'],
+        fromPort: 80,
+        toPort: 80,
+      },
+    ],
+    egress: [
+      // Allow outbound traffic to any destination
+      {
+        protocol: '-1',
+        cidrBlocks: ['0.0.0.0/0'],
+        fromPort: 0,
+        toPort: 0,
+      },
+    ],
+  }
+);
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const graphqlEcsClusterAllowAlbInboundOnly = new aws.ec2.SecurityGroup(
+  'graphql-ecs-allow-alb-inbound',
+  {
+    name: 'graphql-ecs-allow-alb-inbound',
+    description: 'Graphql ECS cluster allow inbound from ALB',
+    ingress: [
+      // Allow inbound traffic only from the ALB
+      {
+        protocol: 'TCP',
+        fromPort: 0,
+        toPort: 65535,
+        securityGroups: [albAllowInboundFromAnywhere.id],
+      },
+    ],
+    egress: [
+      // Allow outbound traffic to any destination
+      {
+        protocol: '-1',
+        fromPort: 0,
+        toPort: 0,
+        cidrBlocks: ['0.0.0.0/0'],
+      },
+    ],
+  }
+);
+
+const repository = new aws.ecr.Repository('graphql-repository', {
+  imageTagMutability: 'MUTABLE',
+  forceDelete: true,
+});
+
+const repoCredentials = repository.registryId.apply(async (registryId) => {
+  const credentials = await aws.ecr.getCredentials({
+    registryId: registryId,
+  });
+  const decodedCredentials = Buffer.from(
+    credentials.authorizationToken,
+    'base64'
+  ).toString();
+  const [username, password] = decodedCredentials.split(':');
+  return { server: credentials.proxyEndpoint, username, password };
+});
+const graphqlImage = new docker.Image('graphql-image', {
+  imageName: pulumi.interpolate`${repository.repositoryUrl}:latest`,
+  registry: repoCredentials,
+  build: {
+    context: '../',
+    dockerfile: '../graphql/Dockerfile',
+    platform: 'linux/amd64',
+    args: {
+      NODE_ENV: 'production',
+      PORT: '8000',
+      BUILD_FLAG: '--production',
+    },
+    cacheFrom: {
+      images: [pulumi.interpolate`${repository.repositoryUrl}:latest`],
+    },
+  },
+});
