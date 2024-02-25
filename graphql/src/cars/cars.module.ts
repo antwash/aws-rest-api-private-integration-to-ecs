5213535c8ef2e568c3f2ece50683c5349afe1f7b
Anthony Washington <awashington251@gmail.com>
2024-02-24 23:12:58 -0500

    remove gql project and infra

--- graphql/src/cars/cars.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CarResolver } from './cars.resolver';
-import { CarsService } from './cars.service';
-
-@Module({
-  providers: [CarResolver, CarsService],
-})
-export class CarsModule {}
